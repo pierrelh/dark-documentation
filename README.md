46617aec0bf16242134559ea41865aab76b3c700
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-03-11 15:50:02 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dark Documentation
+
+Dark Documentation is a simple & light documentation for you codes.
+
+![alt tag](./img/exemple.png)
+
+## Installation:
+
+Clone or download the repo
+
+## To ScreenShot your code:
+
+Use carbon, to create nice screenshots of your code, using this link to improve the readability of the final file:
+
+https://carbon.now.sh/?bg=rgba%28171%2C184%2C195%2C0%29&t=vscode&wt=none&l=text%2Fx-go&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=18px&lh=133%25&si=false&es=1x&wm=false&code=
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+		<link rel="stylesheet" href="./styles/master.css">
+    	<link rel="stylesheet" href="./styles/documentationStyle.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Documentation</title>
+    </head>
+    <body>
+        <ul class="documentation-list">
+            <li class="nav-element">
+                <div class="nav">
+                <!-- Put here the treeview of your code -->
+                    <ul>
+                        <li>
+                            <!-- The name of your file -->
+                            <a class="one" href="#File">File</a>
+                        </li>
+                        <li>
+                            <a class="two" href="#Sub-File">Sub-File</a>
+                        </li>
+                        <li>
+                            <a class="three" href="#Sub-Sub-File">Sub-Sub-File</a>
+                        </li>
+                        <li>
+                            <a class="four" href="#Sub-Sub-File">Sub-Sub-Sub-File</a>
+                        </li>
+                        <li>
+                            <a class="one" href="#File">File</a>
+                        </li>
+                        <li>
+                            <a class="two" href="#Sub-File">Sub-File</a>
+                        </li>
+                        <li>
+                            <a class="three" href="#Sub-Sub-File">Sub-Sub-File</a>
+                        </li>
+                        <li>
+                            <a class="four" href="#Sub-Sub-File">Sub-Sub-Sub-File</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="doc-element">
+                <div class="doc">
+                    <div id="File">
+                        <!-- The name of your file -->
+                        <h1>#File</h1>
+                        <p>
+                            <!-- A quick description of the file purpose -->
+                            A quick description of your file purpose
+                        </p>
+                        <ul>
+                            <li>
+                                <!-- The Screen of your code file -->
+                                <img src="./img/docs/sample.png" alt="">
+                            </li>
+                            <li> 
+                                <!-- A Comment -->
+                                <p style="margin-top: 20%">
+                                    Some comments...
+                                </p>
+                                <p style="margin-top: 20%">
+                                    Some comments...
+                                </p>
+                                <p style="margin-top: 20%">
+                                    Some comments...
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </body>
+</html>
